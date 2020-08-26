02d3d400e6c6e77bcf6ccabbec14ebe308d59528
Anton Liverov <liverov@yandex.ru>
2020-08-26 18:21:30 +0300

    Заполнение моками

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 import {createTripInfoTemplate} from './view/trip-info.js';
 import {createSiteMenuTemplate} from './view/site-menu.js';
-import {addFirstEventTemaplte} from './view/add-first-event.js';
+import {createEditEventTemplate} from './view/edit-event.js';
 import {createFilterTemplate} from './view/filter.js';
 import {createSortTemplate} from './view/sort.js';
 import {createDaysItemTemplate} from './view/days.js';
-import {createDaysEventsTemplate} from './view/days-events.js';
+import {generateEvent} from './mock/event.js';
+// import {generateFilter} from './mock/filter.js';
+import {getDateInDayMonth} from './utils.js';
 
-const DAYS_COUNT = 3;
-const EVENTS_COUNT = 3;
+const EVENTS_COUNT = 20;
+
+const events = new Array(EVENTS_COUNT).fill().map(generateEvent);
+const sortedEvents = events.slice().sort((a, b) => a.startDate - b.startDate);
+// const filters = generateFilter(events);
 
 const siteHeaderElement = document.querySelector(`.page-header`);
 const siteTripInfoElement = siteHeaderElement.querySelector(`.trip-main`);
 const siteControlsElement = siteHeaderElement.querySelector(`.trip-controls`);
 const siteMainElement = document.querySelector(`.page-main`);
 const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
+const dayElement = siteMainElement.querySelector(`.trip-days`);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -22,17 +28,18 @@ const render = (container, template, place) => {
 render(siteTripInfoElement, createTripInfoTemplate(), `afterbegin`);
 render(siteControlsElement, createSiteMenuTemplate(), `afterbegin`);
 render(siteControlsElement, createFilterTemplate(), `beforeend`);
-render(tripEventsElement, addFirstEventTemaplte(), `afterbegin`);
-render(tripEventsElement, createSortTemplate(), `beforeend`);
+render(tripEventsElement, createSortTemplate(), `afterbegin`);
 
-const tripDaysElement = siteMainElement.querySelector(`.trip-days`);
+let listEvents = [];
+for (let i = 0; i < sortedEvents.length - 1; i++) {
+  if (getDateInDayMonth(sortedEvents[i].startDate) !== getDateInDayMonth(sortedEvents[i + 1].startDate)) {
+    const dayMonth = getDateInDayMonth(sortedEvents[i].startDate);
+    const startDateEvent = sortedEvents[i].startDate;
+    listEvents = sortedEvents.filter((event) => dayMonth === getDateInDayMonth(event.startDate));
 
-for (let i = 0; i < DAYS_COUNT; i++) {
-  render(tripDaysElement, createDaysItemTemplate(), `afterbegin`);
-
-  const tripDaysEventsListElement = tripDaysElement.querySelector(`.trip-events__list`);
-
-  for (let j = 0; j < EVENTS_COUNT; j++) {
-    render(tripDaysEventsListElement, createDaysEventsTemplate(), `afterbegin`);
+    render(dayElement, createDaysItemTemplate(startDateEvent, listEvents), `beforeend`);
   }
 }
+
+const dayEventsElement = dayElement.querySelector(`.trip-events__list`);
+render(dayEventsElement, createEditEventTemplate(listEvents[0]), `afterbegin`);
--- src/view/days.js
+++ src/view/days.js
@@ -1,23 +1,30 @@
+import {createListEventsTemplate} from './events.js';
+
 const siteMainElement = document.querySelector(`.page-main`);
 const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
 
 const createDaysTemplate = () => {
-  return (
-    `<ul class="trip-days"></ul>`
-  );
+  return (`<ul class="trip-days"></ul>`);
 };
 
 tripEventsElement.insertAdjacentHTML(`beforeend`, createDaysTemplate());
 
-export const createDaysItemTemplate = () => {
+let dayCounter = 0;
+export const createDaysItemTemplate = (startDateEvent, listEvents) => {
+  dayCounter++;
+
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
-        <span class="day__counter">1</span>
-        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+        <span class="day__counter">${dayCounter}</span>
+        <time class="day__date" datetime="2019-03-18">
+        ${startDateEvent.toLocaleString(`EN-GB`, {month: `short`})} 
+        ${startDateEvent.toLocaleString(`EN-GB`, {day: `numeric`})}
+        </time>
       </div>
 
       <ul class="trip-events__list">
+      ${createListEventsTemplate(listEvents)}
       </ul>
     </li>`
   );
